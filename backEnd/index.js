92f4039dff6698665b75fdd8194d723abc6619cf
Prassadraj <Prassadpr10@gmail.com>
2024-02-24 02:05:57 +0530

    chg

--- backEnd/index.js
+++ backEnd/index.js
@@ -2,13 +2,22 @@ import express, { json } from "express";
 import { PORT,MongoURL} from "./config.js";
 import mongoose from 'mongoose'
 import bookRouter from './routes/bookRouter.js'
+import cors from 'cors'
+import { Bookmodel } from "./models/bookmodel.js";
 
 const app=express()
 //middleWare for getting data from body and to modify
 app.use(express.json())
+//mIddleware for handling CORS policy
+app.use(cors({
+    origin:"http://localhost:3000/",
+    methods:['GET','POST','PUT','DELTE'],
+    allowedHeaders:['Content-Type']
+}))
 
 app.get('/',(req,res)=>{
     res.send(`<h1>helloMan</h1>`).status(202)
+    console.log(req);
 })
 app.use('/books',bookRouter)
 
