aed7001f0181ab161e03d1143c5296f1e5c7039b
Prassad Raj R <prassadpr10@gmail.com>
2024-04-08 23:21:09 +0530

    chg

--- backEnd/config.js
+++ backEnd/config.js
@@ -1,2 +1,2 @@
-export const PORT=5555
-export const MongoURL='mongodb://localhost:27017'
+export const PORT = 5555;
+export const MongoURL = `mongodb+srv://prassadpr10:prassadpr10@cluster0.decbeum.mongodb.net/BookStore?retryWrites=true&w=majority&appName=Cluster0`;
