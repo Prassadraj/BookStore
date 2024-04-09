85a5f6f3bbbdeefdc5a176e08ed0ede2c2f4582c
Prassad Raj R <prassadpr10@gmail.com>
2024-04-09 13:43:32 +0530

    chg

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,29 +1,34 @@
 import express, { json } from "express";
-import { PORT,MongoURL} from "./config.js";
-import mongoose from 'mongoose'
-import bookRouter from './routes/bookRouter.js'
-import cors from 'cors'
+import { PORT, MongoURL } from "./config.js";
+import mongoose from "mongoose";
+import bookRouter from "./routes/bookRouter.js";
+import cors from "cors";
 // import { Bookmodel } from "./models/bookmodel.js";
 
-const app=express()
+const app = express();
 //middleWare for getting data from body and to modify
-app.use(express.json())
+app.use(express.json());
 //mIddleware for handling CORS policy
-app.use(cors())
-app.options('*',cors())
+app.use(cors());
+app.options("*", cors());
+// app.use(cors({
+//     origin:"http://localhost:3000/",
+//     methods:['GET','POST','PUT','DELTE'],
+//     allowedHeaders:['Content-Type']
+// }))
 
-app.get('/',(req,res)=>{
-    res.send(`<h1>helloMan</h1>`).status(202)
-    console.log(req);
-})
-app.use('/books',bookRouter)
+app.get("/", (req, res) => {
+  res.send(`<h1>helloMan</h1>`).status(202);
+  console.log(req);
+});
+app.use("/books", bookRouter);
 
-
-
-mongoose.connect(MongoURL).then(()=>{
-    console.log("MongoDB connected")
-    app.listen(PORT||5000,()=>{
-        console.log(`Port is running ${PORT}`);
-    })
-})
-.catch((err)=>console.log(err))
+mongoose
+  .connect(MongoURL)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT || 5000, () => {
+      console.log(`Port is running ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
